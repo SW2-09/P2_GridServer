5cee6166e8fa9a8b5b8a64c53249ce9a7691645f
od45pv <od45PV@student.aau.dk>
2023-03-27 13:08:19 +0200

    VI KAN UPLOADE FILER!
    Co-authored-by: AndreasGriem <AndreasGriem@users.noreply.github.com>

--- gridServer/routes/buyer.js
+++ gridServer/routes/buyer.js
@@ -2,12 +2,34 @@ const { application } = require("express");
 const express = require("express");
 const buyerRouter = express.Router();
 const path = require("path");
+const fileUpload = require("express-fileupload");
 
 // Guide for sending html: https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
 buyerRouter.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/buyer.html"));
 });
 
-const upload = require("express-fileupload");
+// default options
+buyerRouter.use(fileUpload());
+
+buyerRouter.post("/upload", function (req, res) {
+  let sampleFile;
+  let uploadPath;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  sampleFile = req.files.sampleFile;
+  uploadPath = "../gridServer/public/uploads/" + sampleFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+
+    res.send("File uploaded!");
+  });
+});
 
 module.exports = buyerRouter;
