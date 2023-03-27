d8666ba87e91d85be517ffed4a9b0484eaf1f083
od45pv <od45PV@student.aau.dk>
2023-03-27 15:03:56 +0200

    fileUpload(). Men crasher hver gang :))
    Co-authored-by: AndreasGriem <AndreasGriem@users.noreply.github.com>

--- gridServer/routes/buyer.js
+++ gridServer/routes/buyer.js
@@ -1,34 +1,56 @@
+
 const { application } = require("express");
 const express = require("express");
 const buyerRouter = express.Router();
 const path = require("path");
 const fileUpload = require("express-fileupload");
+const { rejects } = require("assert");
+const { resolve } = require("path");
+
+/* TODO LIST:
+  * 1. Create upload-folder if it doesn't exist
+  * 2. 
+*/
+
+const dirPath = "../gridServer/uploads/";
+const allowedFileFormat = ["csv", "json"];
+const maxFileSize = 10 * 1024 * 1024; // 10 MB
 
 // Guide for sending html: https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files
 buyerRouter.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/buyer.html"));
 });
 
-// default options
+/* ***********************
+ *  File Upload Section *
+ ************************ */
+
 buyerRouter.use(fileUpload());
 
-buyerRouter.post("/upload", function (req, res) {
-  let sampleFile;
-  let uploadPath;
+buyerRouter.post("/upload", (req, res) => {
+  return new Promise((resolve, reject) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+      // If no files were uploaded (i.e. no file was selected)
+      reject(new Error("No files were uploaded."));
+    }
+    const sampleFile = req.files.sampleFile;
+    const uploadPath = dirPath + sampleFile.name;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send("No files were uploaded.");
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = "../gridServer/public/uploads/" + sampleFile.name;
-
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err);
-
-    res.send("File uploaded!");
+    if (!allowedFileFormat.includes(sampleFile.mimetype)) {
+      reject(new Error("File format not supported"));
+    }
+    if (sampleFile.size > maxFileSize) {
+      reject(
+        new Error("File size is too big. Max support is " + maxFileSize + "MB")
+      );
+    }
+    sampleFile.mv(uploadPath, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve("File uploaded!");
+      }
+    });
   });
 });
 
