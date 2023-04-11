82cc012ab70cf57bbfbfde94282291c86e0bd460
EmilChri <113058804+EmilChri@users.noreply.github.com>
2023-04-11 14:34:49 +0200

    vi prøver igen

--- fileReader.js
+++ fileReader.js
@@ -1,82 +1,38 @@
-let matrixA = [];
-let matrixPack = "";
+export {expMatrixA, expMatrixB};
 
-const uploadFile = document.getElementById('uploadComfirmed').addEventListener('click', () => {
-    Papa.parse(document.getElementById('uploadFile').files[0],
-    {
-        download: true, 
-        header: false, 
-        skipEmptyLines: true, //other option is 'greedy', meaning skip delimiters, quotes, and whitespace.
-        complete: function(results){
-            console.log(results);
-            
-            for (i = 0; i < results.data.length; i++) { 
-                matrixA.push(results.data[i]);
+let uploadButton = document.getElementById('uploadButton');
 
-                for (let j = 0; j < matrixA[i].length; j++) {
-                    matrixA[i][j]=Number(matrixA[i][j]);    //Converts the string to a number
-                }
-            }
-            console.log("matrix A")
-            console.log(matrixA);
-           
-            matrixPack += '{ "matrixA": [\n'
-            for (let i = 0; i < matrixA.length; i++) {
-                if (i > 0)
-                    matrixPack += ",\n";
-                matrixPack += ("[" + matrixA[i] + "]") ;
-            }
-            matrixPack += "\n] ";
-            console.log("matrix A JSON")
-            console.log(matrixPack)
-        }   
-        
-    })
-    
+uploadButton.addEventListener('click', 
+() => {
+    let expMatrixA = parseCsvToJson(document.getElementById('uploadFile').files[0]);
+    let expMatrixB = parseCsvToJson(document.getElementById('uploadFile2').files[0]);
 });
-const uploadFile2 = document.getElementById('uploadComfirmed').addEventListener('click', () => {
-    Papa.parse(document.getElementById('uploadFile2').files[0],
+
+
+function parseCsvToJson(file){
+    let matrix = [];
+    Papa.parse(file, // import a file and sets it to uploadFile
     {
         download: true,
         header: false,
         skipEmptyLines: true, //other option is 'greedy', meaning skip delimiters, quotes, and whitespace.
         complete: function(results){
-            console.log(results);
-            
-            for (i = 0; i < results.data.length; i++) { 
-                matrixB.push(results.data[i]);
-
-                for (let j = 0; j < matrixB[i].length; j++) {
-                    matrixB[i][j]=Number(matrixB[i][j]);    //Converts the string to a number
-                }
-            }
-            console.log("matrix B")
-            console.log(matrixB);
+            let placeholder = []; 
+            //the placeholder represents the rows of the matrix which then can be pushed to the matrix array
            
-            matrixPack += ',\n "matrixB": [\n'
-            for (let i = 0; i < matrixB.length; i++) {
-                if (i > 0)
-                    matrixPack += ",\n";
-                matrixPack += ("[" + matrixB[i] + "]") ;
-            }
-            matrixPack += "\n] }";
-            console.log("matrix B JSON")
-            console.log(matrixPack)
+            for (i = 0; i < results.data.length; i++) { 
+                for (let j = 0; j < results.data[i].length; j++) {
+                    if (results.data[i][j])
+                        placeholder.push(parseFloat(results.data[i][j]));
+                }
 
-            let jason = JSON.parse(matrixPack);
-            console.log(jason.matrixA)
-            console.log(jason.matrixB)
-        }   
-        
+                matrix.push(placeholder);
+                placeholder = [];
+            };
+
+            console.log("matrix")
+            console.log(matrix);
+            exportmatrix = matrix;
+        }
     })
-    
-});
- /*  function combinematrix(A,B){
-                return A.concat(B);
-            }
-          */
-         
-            // let combinedMatrix = combinematrix(matrixA,matrixB);
-            // let combinedMatrix2 = combinematrix(matrixC,matrixD);
-            // console.log(combinedMatrix);
-            // console.log(combinedMatrix2);
+}
