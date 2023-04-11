9c4a97f30cf7589cb325ef6807c8b69b945d1153
od45pv <od45PV@student.aau.dk>
2023-04-11 15:34:55 +0200

    Fix af upload
    Co-authored-by: Peterrahr <Peterrahr@users.noreply.github.com>

--- gridServer/routes/index.js
+++ gridServer/routes/index.js
@@ -1,100 +1,95 @@
-export {router}
+export { router };
 
-import express from "express"
+import express from "express";
 import { ensureAuthenticated } from "../config/authentication.js";
-import passport from 'passport'
-import bcrypt from "bcryptjs"
-import {Buyer} from "../models/Buyer.js";
-
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import { Buyer } from "../models/Buyer.js";
 
 const router = express.Router();
 
 //login page
-router.get('/', (req, res)=>{
-    res.render('login')
-})
+router.get("/", (req, res) => {
+  res.render("login");
+});
 
-router.get('/login', (req, res)=>{
-    res.render('login')
-})
+router.get("/login", (req, res) => {
+  res.render("login");
+});
 
 //register page
-router.get('/register', (req, res)=>{
-    res.render('register')
-})
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
 // buyer pag
-router.get('/buyer',ensureAuthenticated, (req, res) =>{
-    res.render('buyer', {name: req.user.name})
-})
+router.get("/buyer", ensureAuthenticated, (req, res) => {
+  res.render("buyer", { name: req.user.name });
+});
 
-router.post('/register', (req,res) =>{
-    const {name, password, password2} = req.body;
-    let errors = [];
-  
-    //Check required fiels
-    if(!name || !password || !password2) {
-     errors.push({msg: 'Please enter a password'});
-    }
-  
-    //Check password match
-    if(password != password2){
-         errors.push({msg: 'Passwords do not match'});
-    }
-  
-    //Check if there is an error in the array
-    if(errors.length > 0){
-      res.render('register',{
-         errors, 
-         name,
-         password,
-         password2,
-      });
-    } else{
-     //validation passed
-        Buyer.findOne({name: name})
-        .then(buyer =>{
-             if(buyer){
-                 //Buyer exists
-                 errors.push({msg: 'Buyer is taken'});
-                 res.render('register',{
-                     errors: errors, 
-                     name: name,
-                     password: password,
-                     password2: password2
-                  });
-             } else{
-                 const newBuyer = new Buyer ({
-                     name: name,
-                     password: password
-                 });
-  
-                 //Check the hashed password. Default function
-                 bcrypt.genSalt(10, (err, salt) => {
-                     bcrypt.hash(newBuyer.password, salt, (err, hash) => {
-                       if (err) throw err;
-                       newBuyer.password = hash;
-                       newBuyer
-                         .save()
-                         .then(buyer => {
-                           res.redirect('/login');
-                         })
-                         .catch(err => console.log(err));
-                     });
-                   });
-             }          
-        })
-    }
-    
-  })
+router.post("/register", (req, res) => {
+  const { name, password, password2 } = req.body;
+  let errors = [];
 
+  //Check required fiels
+  if (!name || !password || !password2) {
+    errors.push({ msg: "Please enter a password" });
+  }
 
-  
+  //Check password match
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  //Check if there is an error in the array
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      password,
+      password2,
+    });
+  } else {
+    //validation passed
+    Buyer.findOne({ name: name }).then((buyer) => {
+      if (buyer) {
+        //Buyer exists
+        errors.push({ msg: "Buyer is taken" });
+        res.render("register", {
+          errors: errors,
+          name: name,
+          password: password,
+          password2: password2,
+        });
+      } else {
+        const newBuyer = new Buyer({
+          name: name,
+          password: password,
+        });
+
+        //Check the hashed password. Default function
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newBuyer.password, salt, (err, hash) => {
+            if (err) throw err;
+            newBuyer.password = hash;
+            newBuyer
+              .save()
+              .then((buyer) => {
+                res.redirect("/login");
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
 //Login handle
-router.post('/login',(req,res,next)=>{
-    console.log(req.body)
-    passport.authenticate('local',{
-        successRedirect: '/buyer',
-        failureRedirect: '/login'
-    })(req, res, next);
-  })
+router.post("/login", (req, res, next) => {
+  console.log(req.body);
+  passport.authenticate("local", {
+    successRedirect: "/buyer",
+    failureRedirect: "/login",
+  })(req, res, next);
+});
