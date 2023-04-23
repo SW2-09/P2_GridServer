d1276eeaa7141ae548409e229ce9a3b9fba1a747
Andreas Valentin Pedersen <91377870+Rosebuddet@users.noreply.github.com>
2023-04-23 15:33:28 +0200

    handlers object at handerls test

--- test/web_socket/handlersTest.spec.js
+++ test/web_socket/handlersTest.spec.js
@@ -1,54 +1,80 @@
+
 import {spy} from "sinon";
 import {assert as assertSinon} from "sinon"
-import {subtaskFeeder} from "../../Jobtypes/matrix_multiplication/taskFeed.js"
+
+import { subtaskFeeder } from "../../Jobtypes/matrix_multiplication/taskFeed.js";
 import{JobQueue} from "../../Jobtypes/matrix_multiplication/jobQueue.js";
-import { WebSocket } from "ws";
-import { WebSocketServer } from "ws";
 import {arr, matrix_B} from "../../Jobtypes/matrix_multiplication/matrixSplit.js"
 
+import { WebSocket } from "ws";
+import { WebSocketServer } from "ws";
+// Udestående: handlers skal importeres frem for defineres i denne fil. Det virker dog ikke umiddelbart. 
+//import {handlers} from "../../web_socket/handlers.js"
+
 let handlers={
-  JobQueue: JobQueue,
-  subtaskFeeder: subtaskFeeder,
-  ws: null,
-  sendSubtask: function sendSubtask(){
-                  //console.log("subtasklist tail:"+JobQueue.tail.subtaskList.tail);
-                  let next_task = handlers.subtaskFeeder(handlers.JobQueue,handlers.ws);
-                  handlers.ws.send(JSON.stringify(next_task));
-                },
-  messageHandler: function messageHandler(message){
-
-                    // Try is for if some one sends somehting which cannot be passed to JSON:
-                    try {
-                    let messageParse = JSON.parse(message);
-                    console.log("Message recieved about this job:");
-                    console.log(
-                      messageParse.jobId
-                      );
-                    } catch (e) {
-                    console.log(`Something went wrong with the recieved message: ${e.message}`);
-                    }
-
-                    try{
-                      handlers.sendSubtask();
-                    } catch (e){
-                    console.log(`Something went wrong with sending message to server: ${e.message}`);
-                    }
-                  },
-connectionHandler: function connectionHandler(ws){
-                      handlers.ws=ws
-                      console.log("New client connected");
-                      console.log("JobQueue: " + JobQueue);
+    subtaskFeeder: subtaskFeeder,
+    ws: null,
+    sendSubtask: function send_subtask() {
+      let next_task = handlers.subtaskFeeder(JobQueue);
+      if (next_task !== null) { //if there is a subtask to send
+        handlers.ws.send(JSON.stringify(next_task));
+      }
+      else{ //if there is no subtask to send
+        console.log("sending 0 to worker")
+        handlers.ws.send("0");
+      }
+    },
+    messageHandler: (message) => { //callback for when a message is recieved from the client
+      // Try is for if some one sends somehting which cannot be passed to JSON:
+      try {
+        let messageParse = JSON.parse(message);
+        
+        if (messageParse["data"] === "ready for work") { //if the worker is ready for work
+        //send_subtask(ws, JobQueue); //send a subtask to the worker
+        
+        }
+        else {
+          let messageParse = JSON.parse(message);
+          console.log("Solution recieved:");
   
-                      if (JobQueue.size > 0) {
-                      
-                      handlers.sendSubtask(ws);
-                      } else {
-                      ws.send("0");
-                      }
+          console.log("jobID: " + messageParse["jobId"]);
+          console.log("taskID: " + messageParse["taskId"]);
+          let currentJob=findJob(messageParse["jobId"]); //find the job in the queue
+          currentJob.solutions[messageParse["taskId"]] = messageParse["solution"]; 
+          currentJob.numOfSolutions++; //increase the number of solutions
+          currentJob.pendingList.removeTask(messageParse["taskId"]); //remove the task from the pending list
+          // console.log("job solutions" + JobQueue.tail.solutions.length);
+          // console.log(messageParse["solution"]);
+        }
+        
+      } catch (e) { //if the message cannot be parsed to JSON
+        console.log(`Something went wrong with the recieved message: ${e.message}`);
+      }
+  
+      try{ //try to send the next subtask
+          handlers.sendSubtask();
+      } catch (e){ //if an error occurs when sending a subtask
+        console.log(`Something went wrong with sending message to server: ${e.message}`);
+      }
+    },
+  connectionHandler: function connectionHandler(ws){//callback for when a new client connects
+    handlers.ws=ws
+    console.log("New client connected");
+    if (JobQueue.size > 0) { //if the queue is not empty, send a subtask to the worker
+    handlers.sendSubtask();
+    } else { //if the queue is empty, send 0 to the worker
+      console.log("sending 0 to worker")
+      ws.send("0");
+    }
+  
+    ws.on("message", handlers.messageHandler)
+  
+    ws.on("close", () => { //when the worker disconnects
+      console.log("Client has disconnected");
+    });
+  }
+  }
 
-                     ws.on("message", handlers.messageHandler)
-                    }
-}
 
 
 describe("handlers", function(){
@@ -149,9 +175,10 @@ describe("handlers", function(){
          JobQueue.head.numOfTasks++;
          }
 
-        handlers.JobQueue.tail.subtaskList.tail=null;
-        let spy_deQueue=spy(handlers.JobQueue,"deQueue");
+        JobQueue.tail.subtaskList.tail=null;
+        let spy_deQueue=spy(JobQueue,"deQueue");
         assertSinon.notCalled(spy_deQueue);
     });
     });
 });
+
