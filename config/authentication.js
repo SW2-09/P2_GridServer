ae2c020f404a735960dc8c93fab1c56d580a3ef6
od45pv <od45PV@student.aau.dk>
2023-04-26 13:30:26 +0200

    Prettier 4 width

--- config/authentication.js
+++ config/authentication.js
@@ -1,16 +1,16 @@
 export { ensureAuthenticated, checkLoggedIn };
 
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/");
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
 }
 
 //Ensures you can not redirect from buyer page to other pages if logged in
 function checkLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/buyer");
-  }
-  next();
+    if (req.isAuthenticated()) {
+        return res.redirect("/buyer");
+    }
+    next();
 }
