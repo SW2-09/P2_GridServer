43f53a6402ae538db546fc6658590c0e39a9b957
Andreas Valentin Pedersen <91377870+Rosebuddet@users.noreply.github.com>
2023-05-11 16:48:36 +0200

    jobQueue test

--- test/Jobtypes/jobQueue.Test.spec.js
+++ test/Jobtypes/jobQueue.Test.spec.js
@@ -1,8 +1,5 @@
-import { spy, assert as assertSinon } from "sinon";
 import { assert } from "chai";
-import { addMatrixToQue } from "../../Jobtypes/matrix_multiplication/jobCreateMatrixMult.js";
-import { subtaskFeeder, jobDone } from "../../Jobtypes/taskFeed.js";
-import { JobQueue, Job } from "../../Jobtypes/jobQueue.js";
+import { JobQueue } from "../../Jobtypes/jobQueue.js";
 import { makeMatrixPair } from "./taskFeedTest.spec.js";
 
 describe("jobQueue.js", function () {
@@ -11,55 +8,65 @@ describe("jobQueue.js", function () {
             JobQueue.deQueue();
         }
     });
-    it("should have size 0 when queue is empty", function () {
+    it("Should have size 0 when queue is empty", function () {
         let expectedSize = 0;
         assert.equal(JobQueue.size, expectedSize);
     });
-    it("should have size 1 after 10 jobs are added to an empty queue adn 9 are removed", function () {
-        for (let i = 1; i < 11; i++) {
-            JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
-        }
 
-        for (let i = 1; i < 10; i++) {
+    describe("enQueue and deQueue", function () {
+        it("Should have size 1 after one job is enqueued in an empty queue", function () {
+
+                JobQueue.enQueue(`testJobId`, "testJobType", "Donald Duck");
+
+            let expectedSize = 1;
+
+            assert.equal(JobQueue.size, expectedSize);
+        });
+        it("Should have size 1 after 10 jobs are added to an empty queue and 9 are removed", function () {
+            for (let i = 1; i < 11; i++) {
+                JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
+            }
+
+            for (let i = 1; i < 10; i++) {
+                JobQueue.deQueue();
+            }
+
+            let expectedSize = 1;
+
+            assert.equal(JobQueue.size, expectedSize);
+        });
+        it("Should have size 0 after a job is added to an empty queue and then dequeued", function () {
+            let matrixPair = makeMatrixPair(2, 2, 2, 10);
+
+            JobQueue.enQueue(`testJobId`, "testJobType", "Donald Duck");
             JobQueue.deQueue();
-        }
+            let expectedSize = 0;
 
-        let expectedSize = 1;
+            assert.equal(JobQueue.size, expectedSize);
+        });
+        it("Head and tail should have jobId of enqueued if enqueued in empty queue", function () {
+            JobQueue.enQueue("testJobId", "testJobType", "Donald Duck");
 
-        assert.equal(JobQueue.size, expectedSize);
-    });
+            let expectedJobId = "testJobId";
 
-    it("should have size 0 after a job is added to an empty queue and then dequeued", function () {
-        let matrixPair = makeMatrixPair(2, 2, 2, 10);
+            assert.equal(JobQueue.head.jobId, expectedJobId);
+            assert.equal(JobQueue.tail.jobId, expectedJobId);
+        });
+        it("Head and tail should have correct jobIds when 10 jobs are enqueued in empty queue", function () {
+            for (let i = 1; i < 11; i++) {
+                JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
+            }
 
-        JobQueue.enQueue(`testJobId`, "testJobType", "Donald Duck");
-        JobQueue.deQueue();
-        let expectedSize = 0;
+            let expectedJobId1 = "testJobId1";
+            let expectedJobId10 = "testJobId10";
 
-        assert.equal(JobQueue.size, expectedSize);
-    });
-    it("Head and tail should have jobID of enqueued, if enqueued in empty queue", function () {
-        JobQueue.enQueue("testJobId", "testJobType", "Donald Duck");
-
-        let expectedJobId = "testJobId";
-
-        assert.equal(JobQueue.head.jobId, expectedJobId);
-        assert.equal(JobQueue.tail.jobId, expectedJobId);
-    });
-    it("Head and tail should have correct jobIds, when 10 jobs are enqueued in empty queue", function () {
-        for (let i = 1; i < 11; i++) {
-            JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
-        }
-
-        let expectedJobId1 = "testJobId1";
-        let expectedJobId10 = "testJobId10";
-
-        assert.equal(JobQueue.tail.jobId, expectedJobId1);
-        assert.equal(JobQueue.head.jobId, expectedJobId10);
+            assert.equal(JobQueue.tail.jobId, expectedJobId1);
+            assert.equal(JobQueue.head.jobId, expectedJobId10);
+        });
     });
 
     describe("removeJob", function () {
-        it("should have size 9 when job number 5 of 10 jobs are removed", function () {
+        it("Should have size 9 when job number 5 of 10 jobs are removed", function () {
             for (let i = 1; i < 11; i++) {
                 JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
             }
@@ -71,7 +78,7 @@ describe("jobQueue.js", function () {
             assert.equal(JobQueue.size, expectedSize);
         });
 
-        it("Should leave queue empty when only job in queue is removed", function () {
+        it("Should leave queue empty when the only job in queue is removed", function () {
             JobQueue.enQueue(`testJobId`, "testJobType", "Donald Duck");
             JobQueue.removeJob(`testJobId`);
 
@@ -104,7 +111,7 @@ describe("jobQueue.js", function () {
             assert.equal(JobQueue.tail.jobId, expectedTailJobId);
         });
 
-        it("Should update .previous.next and .next.previous coorectly if job 3 of 5 jobs is removed", function () {
+        it("Should update job2.next and job4.previous correctly if job3 is removed", function () {
             for (let i = 1; i < 6; i++) {
                 JobQueue.enQueue(`testJobId${i}`, "testJobType", "Donald Duck");
             }
