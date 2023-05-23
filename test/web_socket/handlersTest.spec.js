e70de1b80a57dea197482c769d3a7cdb68a502ad
Andreas Valentin Pedersen <91377870+Rosebuddet@users.noreply.github.com>
2023-05-23 09:11:42 +0200

    Opdatering af websocket test

--- test/web_socket/handlersTest.spec.js
+++ test/web_socket/handlersTest.spec.js
@@ -24,17 +24,8 @@ describe("handlers.js", () => {
     });
 
     describe("connectionHandler", () => {
-        it("should call console.log() once on server connect (with empty JobQueue)", (done) => {
-            let spyLog = spy(console, "log");
-            JobQueue.size = 0;
-            handlers.connectionHandler(client);
-            assertSinon.calledOnce(spyLog);
-            done();
-        });
-
         it('should send "connected"', (done) => {
             let spy_send = spy(client, "send");
-            JobQueue.size = 0;
             handlers.connectionHandler(client);
             assertSinon.calledWith(spy_send, "connected");
             done();
@@ -45,8 +36,8 @@ describe("handlers.js", () => {
         it("should call subtaskFeeder", (done) => {
             let spy_subtaskFeeder = spy(handlers, "subtaskFeeder");
             handlers.sendSubtask(client);
-            done();
             assertSinon.calledOnce(spy_subtaskFeeder);
+            done();
         });
     });
 });
